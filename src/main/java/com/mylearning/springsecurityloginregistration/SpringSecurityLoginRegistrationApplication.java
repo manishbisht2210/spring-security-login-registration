cfa69f6d9c07f03df6338711181785f258c5c7d3
Manish Bisht <manish.bisht@ad.infosys.com>
2018-09-29 16:49:57 +0530

    First Commit

---
+++ src/main/java/com/mylearning/springsecurityloginregistration/SpringSecurityLoginRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.mylearning.springsecurityloginregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLoginRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLoginRegistrationApplication.class, args);
+	}
+}
